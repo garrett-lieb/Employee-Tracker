fdf1668246aa7dc254ebf7738feee0c40bc99ef5
garrett lieb <liebg08@gmail.com>
2024-02-01 17:41:09 -0800

    moved .env file out of db to main to make password not display on github

--- main/server.js
+++ main/server.js
@@ -7,7 +7,8 @@ const db = mysql.createConnection(
     {
         host: '127.0.0.1',
         user: 'root',
-        password: 'wombatcombat',
+        //get password from .env file
+        password: process.env.DB_PASSWORD,
         database: 'employees_db'
     },
     console.log(`Connected to the employee database.`)
